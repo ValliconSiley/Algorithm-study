b9f5bedd8c0b99e8624bc8ca6977141a8cc3dd4c
Ring-wdr <akswnd55@gmail.com>
2022-07-01 22:30:44 +0900

    code: Solve boj 02839 설탕 배달 (MJ)

--- BOJ/DP/02839-설탕_배달/02839-설탕_배달-MJ.java
+++ BOJ/DP/02839-설탕_배달/02839-설탕_배달-MJ.java
@@ -1,1 +1,19 @@
 // git commit -m "code: Solve boj 02839 설탕 배달 (MJ)"
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int fiveMax = N / 5;
+        int times = 5000;
+        for (int i = 0; i <= fiveMax; i++){       
+            int threeMax = (N - i*5)  / 3;
+            for (int j = 0; j <= threeMax; j++){
+                if ((i*5 +j*3) == N && (i+j)<times) times = i+j;
+            }
+        }
+        System.out.println(times == 5000 ? -1 : times);
+    }
+}
